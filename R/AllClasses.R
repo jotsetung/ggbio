13390559d19058f4c294efcb5a3763033ef5677e
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-13 21:46:02 +0000

    Creating branch for BioC 3.0 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/ggbio@95441 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/AllClasses.R
@@ -0,0 +1,25 @@
+setOldClass("options")
+setOldClass("unit")
+setOldClass("gtable")
+setOldClass("theme")
+setOldClass("gTree")
+setOldClass("grob")
+setClassUnion("themeORNULL", c("theme", "NULL"))
+setClassUnion("optionsORNULL", c("options", "NULL"))
+setClassUnion("numericORunit", c("numeric", "unit"))
+setClassUnion("numericORNULL", c("numeric", "NULL"))
+
+setClassUnion("GRangesORNULL", c("GRanges", "NULL"))
+
+
+
+setOldClass("ggplot")
+setClassUnion("ggplotORNULL", c("ggplot", "NULL"))
+setOldClass(c("gg", "ggplot"))
+setClassUnion("ggORNULL", c("gg", "NULL"))
+
+setOldClass("grob")
+setOldClass("trellis")
+setOldClass("lattice")
+
+
